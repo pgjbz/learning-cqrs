f518b35c644722ab024b21a563aa7a87d2ed0dd0
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2023-08-02 13:45:13 -0300

    feat: create projects

---
+++ account.common/src/test/java/dev/pgjbz/account/common/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pgjbz.account.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
