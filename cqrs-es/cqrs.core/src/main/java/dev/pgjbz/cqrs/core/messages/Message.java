2229e162bab2205b9ea4796e5a6c077e8f8466a5
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2023-08-02 15:54:53 -0300

    refactoring: remove useless imports

--- cqrs-es/cqrs.core/src/main/java/dev/pgjbz/cqrs/core/messages/Message.java
+++ cqrs-es/cqrs.core/src/main/java/dev/pgjbz/cqrs/core/messages/Message.java
@@ -1,12 +1,5 @@
 package dev.pgjbz.cqrs.core.messages;
 
-
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Value;
-
-
-
 public interface Message {
     String id();
 }
