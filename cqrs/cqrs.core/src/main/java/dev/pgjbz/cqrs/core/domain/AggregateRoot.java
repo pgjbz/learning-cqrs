498b3051ef07ba438fa11c2875b3d0d726d08db9
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2023-08-28 17:40:28 -0300

    feat: aggregate root

---
+++ cqrs/cqrs.core/src/main/java/dev/pgjbz/cqrs/core/domain/AggregateRoot.java
@@ -0,0 +1,47 @@
+package dev.pgjbz.cqrs.core.domain;
+
+import dev.pgjbz.cqrs.core.events.BaseEvent;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Getter
+public abstract class AggregateRoot {
+
+    protected String id;
+    @Setter
+    private int version = -1;
+    private final List<BaseEvent> changes = new ArrayList<>();
+
+    public void markChangesAsCommitted() {
+        this.changes.clear();
+    }
+
+    protected void applyChange(final BaseEvent event, final boolean isNewEvent) {
+        try {
+            final var method = getClass().getDeclaredMethod("apply", event.getClass());
+            method.setAccessible(true);
+            method.invoke(this, event);
+        } catch (NoSuchMethodException ex) {
+            log.warn("the apply method was not found in the aggretate for {}", event.getClass().getName());
+        } catch (Exception e) {
+            log.error("error apply event to aggregate", e);
+        } finally {
+            if(isNewEvent) {
+                changes.add(event);
+            }
+        }
+    }
+
+    public void raiseEvent(final BaseEvent event) {
+        applyChange(event, true);
+    }
+    public void replayEvents(final Iterable<BaseEvent> events) {
+        events.forEach(event -> applyChange(event, false));
+    }
+
+}
