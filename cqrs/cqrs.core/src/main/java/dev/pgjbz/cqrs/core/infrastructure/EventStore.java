b3d7349273f0d1f91b53a73ae65b1f9bece009b4
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2023-08-29 14:31:55 -0300

    feat: event store interface

---
+++ cqrs/cqrs.core/src/main/java/dev/pgjbz/cqrs/core/infrastructure/EventStore.java
@@ -0,0 +1,10 @@
+package dev.pgjbz.cqrs.core.infrastructure;
+
+import dev.pgjbz.cqrs.core.events.BaseEvent;
+
+import java.util.List;
+
+public interface EventStore {
+    void saveEvents(final String aggregateId, final Iterable<BaseEvent> events, final int expectedVersion);
+    List<BaseEvent> getEvent(final String aggregateId);
+}
